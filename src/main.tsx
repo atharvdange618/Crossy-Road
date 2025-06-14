60912e365036617368c00e7e0edaeb134d9676e7
Atharv Dange <atharvdange618@gmail.com>
2025-06-14 22:28:02 +0530

    feat: initialize project with Vite, React, and Three.js
    - Added package.json with dependencies for React, Three.js, and development tools.
    - Developed Camera, Player, and Renderer components for Three.js integration.
    - Included basic lighting and scene setup in main entry file.

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { Player } from "./components/Player";
+import "./index.css";
+
+const player = Player();
+
+const scene = new THREE.Scene();
+scene.add(player);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight);
+
+const camera = Camera();
+scene.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
